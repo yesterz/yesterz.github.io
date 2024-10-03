64e2aefeda4b0553d98b21fe95552adb9927b99b
yesterz <yesterz@163.com>
2024-10-03 16:29:21 +0800

    去掉“)”

---
+++ _posts/TODO_git配置免密和ssh免密.sh
@@ -0,0 +1,17 @@
+
+```java
+return new Response<String>(1001, url);
+```
+
+Explicit type argument String can be replaced with <> 
+
+explicit adj. clear and exact 清楚明白的；明確的；不含糊的
+
+```java
+Map<BigInteger, String> allCategoryIdAndName = null;
+```
+
+Variable 'allCategoryIdAndName' initializer 'null' is redundant 
+
+redundant adjective (especially of a word, phrase, etc.) unnecessary because it is more than is needed （尤指词、短语等）多余的，不需要的，累赘的，啰唆的
+
